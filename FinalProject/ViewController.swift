a137648a7f52602edc92adc00bcb1f91d0c1d394
Colton Avila <cca753@lib-pclem023.lib.utexas.edu>
2018-03-27 16:33:25 -0500

    Update Core Data

--- FinalProject/ViewController.swift
+++ FinalProject/ViewController.swift
@@ -14,17 +14,6 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        print("hello world")
-        print("pull test")
-        
-        
-        
-        //aslfkjdsalkfj
-        
-        //sdalfkjdsoioewoi
-        //aslkdm
-        //another pull test
-    
     }
 
     override func didReceiveMemoryWarning() {
@@ -32,7 +21,6 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    //Colton's Branch
     
 }
 
